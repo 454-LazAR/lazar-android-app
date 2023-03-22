5a76368b2a0240e461d16c2f921871895d06442a
Gabriel Friederichs <gfrieder101@gmail.com>
2023-03-22 17:58:45 -0500

    Framework for StartActivity with username entry and ListView display

--- app/src/main/java/com/example/lazar_android_app/StartActivity.java
+++ app/src/main/java/com/example/lazar_android_app/StartActivity.java
@@ -3,12 +3,25 @@ package com.example.lazar_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class StartActivity extends AppCompatActivity {
+
+    ArrayList<String> usernames;
+    ArrayAdapter<String> adapter;
+    ListView roster;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
 
         Bundle extras = getIntent().getExtras();
 
@@ -17,16 +30,44 @@ public class StartActivity extends AppCompatActivity {
         } else {
             startAsJoin(extras.getString("roomCode"));
         }
+
+        // update room roster
+        roster = findViewById(R.id.roster);
+        usernames = new ArrayList<>(); // TODO: update "usernames" with pull from database
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                usernames);
+        roster.setAdapter(adapter);
     }
 
     private void startAsHost() {
-        setContentView(R.layout.activity_start_host);
+        // generate room code on server and set room code view text
+        String roomCode = "Loading..."; // TODO: get string from server
+        TextView roomCodeView = findViewById(R.id.roomCode);
+        roomCodeView.setText(roomCode);
+
+        // SHOW "START" BUTTON but disable it (enables when usernames.count > 0)
     }
 
     private void startAsJoin(String roomCode) {
-        setContentView(R.layout.activity_start_join);
+        TextView roomCodeView = findViewById(R.id.roomCode);
+        roomCodeView.setText(roomCode);
+    }
 
-        TextView banner = findViewById(R.id.lobbyBanner);
-        banner.setText(roomCode);
+    public void submitNickname(View view) {
+        EditText roomCodeField = findViewById(R.id.enterNickname);
+        Button joinButton = findViewById(R.id.joinButton);
+        String username = roomCodeField.getText().toString();
+    
+        usernames.add(username);
+
+        // hide the username entry field and join button
+        roomCodeField.setVisibility(View.GONE);
+        joinButton.setVisibility(View.GONE);
+
+        // notify roster's adapter that
+        // data in list is updated to
+        // update our list view.
+        adapter.notifyDataSetChanged();
     }
 }
